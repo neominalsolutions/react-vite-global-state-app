aa344998f20a54503be5a971f21f1494741b9e9a
neominal.solutions <neominal.solutions@gmail.com>
2025-04-08 14:08:49 +0300

    feat: add Todo model and implement async fetchTodos functionality in todo slice

---
+++ src/redux/todos/todo.slice.ts
@@ -0,0 +1,55 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Todo } from '../../models/todo.model';
+
+export interface TodoState {
+	data: Todo[]; // asenkron çalışırken data ismini kullanalım
+	loading: boolean;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	error: any;
+	fetched: boolean;
+}
+
+const initState: TodoState = {
+	data: [],
+	loading: false,
+	error: null,
+	fetched: false,
+};
+
+export const fetchTodos = createAsyncThunk('FETCH-TODOS', async () => {
+	return fetch('https://jsonplaceholder.typicode.com/todos').then((response) =>
+		response.json()
+	);
+});
+
+createSlice({
+	name: 'TODO-ASYNC',
+	initialState: initState,
+	reducers: {
+		// senkron çalışan state ifadeleri için kullanılan yapı
+	},
+	extraReducers(builder) {
+		// aseknron ifadeler için ise kullanalım
+		builder.addCase(fetchTodos.pending, (state: TodoState) => {
+			state.loading = true; // verinin çekilme anında loading true olsun
+		});
+		builder.addCase(
+			fetchTodos.fulfilled,
+			(state: TodoState, action: PayloadAction<Todo[]>) => {
+				state.loading = false;
+				state.fetched = true;
+				state.data = action.payload;
+			}
+		);
+		builder.addCase(
+			fetchTodos.rejected,
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			(state: TodoState, action: PayloadAction<any>) => {
+				state.loading = false;
+				state.fetched = false;
+				state.data = [];
+				state.error = action.payload;
+			}
+		);
+	},
+});
